3da8d5331fc070701e28bfc5f2cb23e70989c060
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-10-18 20:25:24 +0530

    Add files via upload

---
+++ Practical 07-Date-Array-and-pattern/Practical-07-Task-7-pattern-right-half-pyramid/pattern_right_half_pyramid.cpp
@@ -0,0 +1,39 @@
+/*
+	program to print the pattern of inverted right half pyramid
+				*
+			*	*
+		*	*	*
+	*	*	*	*
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j;
+	char arr[4][4];
+	//loop to store the pattern in an array
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			if(i+j>=3)
+			{
+				arr[i][j]='*';
+			}
+			else
+			{
+				arr[i][j]=' ';
+			}
+		}
+	}
+	//loop to print an array
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			cout<<arr[i][j]<<' ';
+		}
+		cout<<endl;
+	}
+	return 0;
+}
