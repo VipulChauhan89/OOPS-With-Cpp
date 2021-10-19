4ce30b0ba59c87edadcb0048a5a560282a1f87b1
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-10-19 20:14:24 +0530

    Add files via upload

---
+++ Practical 07-Date-Array-and-pattern/Practical-07-Task-4-Restore-same-values-in-same-array/v3-Use arr[i][j]-(2*i+j)/restore_using_2_multiplied_by_i_plus_j.cpp
@@ -0,0 +1,35 @@
+/*
+for particular matrix
+1	2	3
+4	5	6
+7	8	9
+*/
+#include <iostream>
+using namespace std;
+int main()
+{	
+	int arr[3][3] = {{1, 2, 3}, 
+						   {4, 5, 6}, 
+						   {7, 8, 9}};
+	cout << "Array is : "<< endl;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}	
+	cout<<endl;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+			arr[i][j] = arr[i][j] - (2*i+j);
+	}	
+	cout << "Modified Array is : " << endl;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}		    
+	return 0;
+}
