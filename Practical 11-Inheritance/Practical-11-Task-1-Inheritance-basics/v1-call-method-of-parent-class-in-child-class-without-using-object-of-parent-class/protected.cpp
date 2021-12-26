2b7cc5e14f8abe50a99db9e783bc92d9c5c92b51
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-12-26 12:33:13 +0530

    Add files via upload

---
+++ Practical 11-Inheritance/Practical-11-Task-1-Inheritance-basics/v1-call-method-of-parent-class-in-child-class-without-using-object-of-parent-class/protected.cpp
@@ -0,0 +1,26 @@
+//call the parent class method in child class function without creating an object of parent class
+#include<iostream>
+using namespace std;
+class parent
+{
+    protected:
+        void display_parent()
+        {
+            cout<<"Class parent method called." << endl;
+        }
+};
+class child : public parent
+{
+    public:
+        void display_child()
+        {
+            cout<<"Calling of class parent method from class child method without creating the object of class parent" << endl;
+            display_parent();
+        }
+};
+int main()
+{
+    child a;
+    a.display_child();
+    return 0;
+}
