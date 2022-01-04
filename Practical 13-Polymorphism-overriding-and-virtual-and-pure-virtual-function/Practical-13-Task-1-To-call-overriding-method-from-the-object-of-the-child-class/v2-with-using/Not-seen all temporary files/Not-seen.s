551b86a8fa00a55ac479257e849321df90e7c5aa
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-01-04 20:02:08 +0530

    Add files via upload

---
+++ Practical 13-Polymorphism-overriding-and-virtual-and-pure-virtual-function/Practical-13-Task-1-To-call-overriding-method-from-the-object-of-the-child-class/v2-with-using/Not-seen all temporary files/Not-seen.s
@@ -0,0 +1,301 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 11, 0	sdk_version 12, 1
+	.section	__TEXT,__literal4,4byte_literals
+	.p2align	2                               ## -- Begin function main
+LCPI0_0:
+	.long	0x3f8ccccd                      ## float 1.10000002
+LCPI0_1:
+	.long	0x400ccccd                      ## float 2.20000005
+	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	leaq	-8(%rbp), %rdi
+	movl	$1, %esi
+	movl	$2, %edx
+	callq	__ZN5Child3addEii
+	movl	%eax, %esi
+	movq	__ZNSt3__14coutE@GOTPCREL(%rip), %rdi
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEElsEi
+	movq	%rax, %rdi
+	movq	__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_@GOTPCREL(%rip), %rsi
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEElsEPFRS3_S4_E
+	movss	LCPI0_0(%rip), %xmm0            ## xmm0 = mem[0],zero,zero,zero
+	movss	LCPI0_1(%rip), %xmm1            ## xmm1 = mem[0],zero,zero,zero
+	leaq	-8(%rbp), %rdi
+	callq	__ZN4Base3addEff
+	movq	__ZNSt3__14coutE@GOTPCREL(%rip), %rdi
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEElsEf
+	movq	%rax, %rdi
+	movq	__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_@GOTPCREL(%rip), %rsi
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEElsEPFRS3_S4_E
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.globl	__ZN5Child3addEii               ## -- Begin function _ZN5Child3addEii
+	.weak_definition	__ZN5Child3addEii
+	.p2align	4, 0x90
+__ZN5Child3addEii:                      ## @_ZN5Child3addEii
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movq	%rdi, -8(%rbp)
+	movl	%esi, -12(%rbp)
+	movl	%edx, -16(%rbp)
+	movl	-12(%rbp), %eax
+	addl	-16(%rbp), %eax
+	addl	$1, %eax
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.p2align	4, 0x90                         ## -- Begin function _ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEElsEPFRS3_S4_E
+__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEElsEPFRS3_S4_E: ## @_ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEElsEPFRS3_S4_E
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-8(%rbp), %rdi
+	callq	*-16(%rbp)
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.globl	__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_ ## -- Begin function _ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_
+	.weak_definition	__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_
+	.p2align	4, 0x90
+__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_: ## @_ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movq	%rax, -16(%rbp)                 ## 8-byte Spill
+	movq	-8(%rbp), %rdi
+	movq	(%rdi), %rax
+	movq	-24(%rax), %rax
+	addq	%rax, %rdi
+	movl	$10, %esi
+	callq	__ZNKSt3__19basic_iosIcNS_11char_traitsIcEEE5widenEc
+	movq	-16(%rbp), %rdi                 ## 8-byte Reload
+	movsbl	%al, %esi
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEE3putEc
+	movq	-8(%rbp), %rdi
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEE5flushEv
+	movq	-8(%rbp), %rax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.globl	__ZN4Base3addEff                ## -- Begin function _ZN4Base3addEff
+	.weak_definition	__ZN4Base3addEff
+	.p2align	4, 0x90
+__ZN4Base3addEff:                       ## @_ZN4Base3addEff
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movq	%rdi, -8(%rbp)
+	movss	%xmm0, -12(%rbp)
+	movss	%xmm1, -16(%rbp)
+	movss	-12(%rbp), %xmm0                ## xmm0 = mem[0],zero,zero,zero
+	addss	-16(%rbp), %xmm0
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.p2align	4, 0x90                         ## -- Begin function _ZNKSt3__19basic_iosIcNS_11char_traitsIcEEE5widenEc
+__ZNKSt3__19basic_iosIcNS_11char_traitsIcEEE5widenEc: ## @_ZNKSt3__19basic_iosIcNS_11char_traitsIcEEE5widenEc
+Lfunc_begin0:
+	.cfi_startproc
+	.cfi_personality 155, ___gxx_personality_v0
+	.cfi_lsda 16, Lexception0
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$64, %rsp
+	movb	%sil, %al
+	movq	%rdi, -8(%rbp)
+	movb	%al, -9(%rbp)
+	movq	-8(%rbp), %rsi
+	leaq	-24(%rbp), %rdi
+	movq	%rdi, -56(%rbp)                 ## 8-byte Spill
+	callq	__ZNKSt3__18ios_base6getlocEv
+	movq	-56(%rbp), %rdi                 ## 8-byte Reload
+Ltmp0:
+	callq	__ZNSt3__1L9use_facetINS_5ctypeIcEEEERKT_RKNS_6localeE
+	movq	%rax, %rcx
+Ltmp1:
+	movq	%rcx, -48(%rbp)                 ## 8-byte Spill
+	jmp	LBB5_1
+LBB5_1:
+	movq	-48(%rbp), %rdi                 ## 8-byte Reload
+	movsbl	-9(%rbp), %esi
+Ltmp2:
+	callq	__ZNKSt3__15ctypeIcE5widenEc
+	movb	%al, %cl
+Ltmp3:
+	movb	%cl, -57(%rbp)                  ## 1-byte Spill
+	jmp	LBB5_2
+LBB5_2:
+	leaq	-24(%rbp), %rdi
+	callq	__ZNSt3__16localeD1Ev
+	movb	-57(%rbp), %al                  ## 1-byte Reload
+	movsbl	%al, %eax
+	addq	$64, %rsp
+	popq	%rbp
+	retq
+LBB5_3:
+Ltmp4:
+	movq	%rdx, %rcx
+	movq	%rax, %rsi
+                                        ## kill: def $ecx killed $ecx killed $rcx
+	movq	%rsi, -32(%rbp)
+	movl	%ecx, -36(%rbp)
+Ltmp5:
+	leaq	-24(%rbp), %rdi
+	callq	__ZNSt3__16localeD1Ev
+Ltmp6:
+	jmp	LBB5_4
+LBB5_4:
+	jmp	LBB5_5
+LBB5_5:
+	movq	-32(%rbp), %rdi
+	callq	__Unwind_Resume
+LBB5_6:
+Ltmp7:
+	movq	%rax, %rdi
+	callq	___clang_call_terminate
+Lfunc_end0:
+	.cfi_endproc
+	.section	__TEXT,__gcc_except_tab
+	.p2align	2
+GCC_except_table5:
+Lexception0:
+	.byte	255                             ## @LPStart Encoding = omit
+	.byte	155                             ## @TType Encoding = indirect pcrel sdata4
+	.uleb128 Lttbase0-Lttbaseref0
+Lttbaseref0:
+	.byte	1                               ## Call site Encoding = uleb128
+	.uleb128 Lcst_end0-Lcst_begin0
+Lcst_begin0:
+	.uleb128 Lfunc_begin0-Lfunc_begin0      ## >> Call Site 1 <<
+	.uleb128 Ltmp0-Lfunc_begin0             ##   Call between Lfunc_begin0 and Ltmp0
+	.byte	0                               ##     has no landing pad
+	.byte	0                               ##   On action: cleanup
+	.uleb128 Ltmp0-Lfunc_begin0             ## >> Call Site 2 <<
+	.uleb128 Ltmp3-Ltmp0                    ##   Call between Ltmp0 and Ltmp3
+	.uleb128 Ltmp4-Lfunc_begin0             ##     jumps to Ltmp4
+	.byte	0                               ##   On action: cleanup
+	.uleb128 Ltmp3-Lfunc_begin0             ## >> Call Site 3 <<
+	.uleb128 Ltmp5-Ltmp3                    ##   Call between Ltmp3 and Ltmp5
+	.byte	0                               ##     has no landing pad
+	.byte	0                               ##   On action: cleanup
+	.uleb128 Ltmp5-Lfunc_begin0             ## >> Call Site 4 <<
+	.uleb128 Ltmp6-Ltmp5                    ##   Call between Ltmp5 and Ltmp6
+	.uleb128 Ltmp7-Lfunc_begin0             ##     jumps to Ltmp7
+	.byte	1                               ##   On action: 1
+	.uleb128 Ltmp6-Lfunc_begin0             ## >> Call Site 5 <<
+	.uleb128 Lfunc_end0-Ltmp6               ##   Call between Ltmp6 and Lfunc_end0
+	.byte	0                               ##     has no landing pad
+	.byte	0                               ##   On action: cleanup
+Lcst_end0:
+	.byte	1                               ## >> Action Record 1 <<
+                                        ##   Catch TypeInfo 1
+	.byte	0                               ##   No further actions
+	.p2align	2
+                                        ## >> Catch TypeInfos <<
+	.long	0                               ## TypeInfo 1
+Lttbase0:
+	.p2align	2
+                                        ## -- End function
+	.section	__TEXT,__text,regular,pure_instructions
+	.p2align	4, 0x90                         ## -- Begin function _ZNSt3__1L9use_facetINS_5ctypeIcEEEERKT_RKNS_6localeE
+__ZNSt3__1L9use_facetINS_5ctypeIcEEEERKT_RKNS_6localeE: ## @_ZNSt3__1L9use_facetINS_5ctypeIcEEEERKT_RKNS_6localeE
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movq	__ZNSt3__15ctypeIcE2idE@GOTPCREL(%rip), %rsi
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rdi
+	callq	__ZNKSt3__16locale9use_facetERNS0_2idE
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.p2align	4, 0x90                         ## -- Begin function _ZNKSt3__15ctypeIcE5widenEc
+__ZNKSt3__15ctypeIcE5widenEc:           ## @_ZNKSt3__15ctypeIcE5widenEc
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movb	%sil, %al
+	movq	%rdi, -8(%rbp)
+	movb	%al, -9(%rbp)
+	movq	-8(%rbp), %rdi
+	movb	-9(%rbp), %cl
+	movq	(%rdi), %rax
+	movsbl	%cl, %esi
+	callq	*56(%rax)
+	movsbl	%al, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.private_extern	___clang_call_terminate ## -- Begin function __clang_call_terminate
+	.globl	___clang_call_terminate
+	.weak_definition	___clang_call_terminate
+	.p2align	4, 0x90
+___clang_call_terminate:                ## @__clang_call_terminate
+## %bb.0:
+	pushq	%rax
+	callq	___cxa_begin_catch
+	callq	__ZSt9terminatev
+                                        ## -- End function
+.subsections_via_symbols
