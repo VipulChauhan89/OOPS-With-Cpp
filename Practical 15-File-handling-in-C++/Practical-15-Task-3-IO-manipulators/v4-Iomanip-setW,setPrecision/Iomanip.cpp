c8449518ca45fa60f5cd1ec981c5faac8285633e
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-01-07 20:35:33 +0530

    Add files via upload

---
+++ Practical 15-File-handling-in-C++/Practical-15-Task-3-IO-manipulators/v4-Iomanip-setW,setPrecision/Iomanip.cpp
@@ -0,0 +1,13 @@
+/*
+	program to use IO manipulators
+	Iomanip:setW,setPrecision
+*/
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	float a=100.0245;
+	cout<<setw(10)<<setprecision(4)<<fixed<<a<<endl;
+	return 0;
+}
